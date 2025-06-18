9204cb6c14519aab3bb4d39373d9c5b63c62fc75
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-06-18 01:24:54 +0100

    .

--- src/pages/api/auth/[nextauth].ts
+++
@@ -1,5 +0,0 @@
-// src/pages/api/auth/[...nextauth].ts
-import NextAuth from "next-auth";
-import { authOptions } from "../../../../lib/auth"; // adjust path accordingly
-
-export default NextAuth(authOptions);
