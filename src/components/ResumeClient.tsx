37dac081f00228abf4ef5ef2da3418b60b228266
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-06-25 09:26:28 +0100

    .

--- src/components/ResumeClient.tsx
+++ src/components/ResumeClient.tsx
@@ -173,7 +173,7 @@ export default function ResumeClient() {
         </p>
         <div className="mb-5">
           <Link
-            className="bg-white/80 rounded-[3px] text-black px-4 py-2"
+            className="bg-white/80 dark:bg-black/80 dark:text-white rounded-[3px] text-black px-4 py-2"
             href={"/profile"}
           >
             Profile
