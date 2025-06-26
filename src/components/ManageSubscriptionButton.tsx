0c5a18368fdc3786ad2ecbe240e3955c2ca5d156
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-06-26 10:00:22 +0100

    .

--- src/components/ManageSubscriptionButton.tsx
+++ src/components/ManageSubscriptionButton.tsx
@@ -12,7 +12,7 @@ export default function ManageSubscriptionButton() {
 
   return (
     <button
-      className="border-1 cursor-pointer text-sm py-2 px-4"
+      className="border-1 cursor-pointer rounded-[3px] text-sm py-2 px-4"
       onClick={handleClick}
     >
       Manage Subscription
